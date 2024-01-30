891157bcae8c68f08b025836dee3b23f3a32bc51
0jorojo <nikita.kulakov@tktk.ee>
2024-01-30 17:04:22 +0200

    create new app

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿//I am a comment
+//ask for a name - greet by name
+
+Console.WriteLine("Enter your name:");
+string userName = Console.ReadLine();
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");
